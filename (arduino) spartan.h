372400cd36a69f5fe3c79e0d03e7ab0b9d0a1d35
marcinliskowski <72291236+marcinliskowski@users.noreply.github.com>
2020-10-20 12:27:27 +0200

    Update (arduino) spartan.h
    Takie bardziej ogólne zmiany wiążące się z dobrymi praktykami programistycznymi.

--- (arduino) spartan.h
+++ (arduino) spartan.h
@@ -2,10 +2,10 @@
 /* Mikołaj Jędrzejczak          */
 /* 19.10.2020                   */
 
-/* Pliki nagłówkowe */
+/* Pliki nagłówkowe */ /* Biblioteki */
 #include <Arduino.h>
 
-/* Definicje */
+/* Definicje */ /* Jest to poprawna konstrukacja kodu, jednak dobrą praktyką jest opisywanie w komentarzach co jest za co odpowiedzialne i jak działa */
 #define SL_K 4
 #define SL_PMW 5
 #define SP_K 9
@@ -13,15 +13,16 @@
 #define PRZYCISK 2
 
 // NodeMCU -> Ardunio
-#define SYGNAL_PRZ 7
-#define SYGNAL_TYL 8
-#define SYGNAL_LEWO 12
+// jPrzy budowaniu opisu połączeń dobrą praktyką jest np coś takiego
+#define SYGNAL_PRZ 7 // NodeMCU -> Arduino - jesli wysylasz cos do arduino
+#define SYGNAL_TYL 8 // NodeMCU <-> Arduino - gdy komunikacja jest w obie strony
+#define SYGNAL_LEWO 12 // NodeMCU <- Arduino - gdy scigasz dane do Noda
 #define SYGNAL_PRAWO 13
 
 
 /* Definicje funkcji */
 uint8_t STOP( void );
-uint8_t PRZOD( uint8_t moc );
+uint8_t PRZOD( uint8_t moc ); // przy używanych wielkościach w komentarzu pisze się jednostki w jakich się posługuje w projekcie
 uint8_t TYL( uint8_t moc );
 uint8_t OBROT( uint8_t PK, uint8_t PPMW, uint8_t LK, uint8_t LPMW );
 uint16_t POMIAR( void );
