965a2e1f9a6ea5b9e20ddaad636d760f5061f14d
Mike8473 <73116172+Mike8473@users.noreply.github.com>
2020-10-30 15:18:51 +0100

    Update (ESP8266) main.cpp

--- (ESP8266) main.cpp
+++ (ESP8266) main.cpp
@@ -1,39 +1,51 @@
-
-/* Biblioteki */
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include "pin.h"
 
+#define BLYNK_PRINT Serial
+char auth[] = "3AG8OrjK4W-8UABPL3DeNeLSDqbmFLvb";
+char ssid[] = "TOK-B8";
+char pass[] = "uwvw9954";
+
+BlynkTimer timer;
+
+void myTimerEvent()
+{
+  uint16_t pomiar;
+  pomiar = analogRead(LIGHT);
+  Serial.println(pomiar);
+  Blynk.virtualWrite(V5, pomiar);
+}
+
+
 void setup() {
- 
+  Serial.begin(9600);
+
+  //Deklaracja pinów
+  pinMode(PRZOD, 1);
+  pinMode(TYL, 1);
+  pinMode(LEWO, 1);
+  pinMode(PRAWO, 1);
+  pinMode(AUTO, 1);
+  pinMode(Vplus, 1);
+  pinMode(Vminus, 1);
+  pinMode(LIGHT, 0);
+
+  digitalWrite(PRZOD, 0);
+  digitalWrite(TYL, 0);
+  digitalWrite(LEWO, 0);
+  digitalWrite(PRAWO, 0);
 
   // Blynk - komunikacja telfon -> NodeMCU
-  pinMode(PRZOD, 1);
-  digitalWrite(PRZOD, 0);
-  pinMode(TYL, 1);
-  digitalWrite(PRZOD, 0);
-  pinMode(LEWO, 1);
-  digitalWrite(PRZOD, 0);
-  pinMode(PRAWO, 1);
-  digitalWrite(PRZOD, 0);
-  pinMode(AUTO, 1);
   Blynk.begin(auth, ssid, pass);
 
-  // Wi-Fi
-  WiFi.begin(SSID, PASS);
-  pinMode(LED, 1);
-  while( WiFi.status() != WL_CONNECTED)
-  {
-  digitalWrite(LED, 1);
-  delay(100);
-  digitalWrite(LED, 0);
-  delay(100);
-  }
+  timer.setInterval(1000L, myTimerEvent);
+  
 }
 
 void loop() {
   Blynk.run();
-  if(WiFi.status() == WL_CONNECTED) digitalWrite(LED, 0);
-  else digitalWrite(LED, 1);
+  timer.run();
+  
+
 }
