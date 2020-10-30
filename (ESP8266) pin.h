ef7b8722cdb8e94085cd3cf3f3afe7e0d227ec9d
Mike8473 <73116172+Mike8473@users.noreply.github.com>
2020-10-30 15:19:17 +0100

    Update (ESP8266) pin.h

--- (ESP8266) pin.h
+++ (ESP8266) pin.h
@@ -1,35 +1,31 @@
-/* Redefinicja pinów nodeMCU */
+/* Redefinicja pinów wemos d1 mini */
 /* Mikołaj Jędrzejczak       */
 /* 20.10.2020                */
 
 /* Pliki nagłówkowe */
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
 /* Definicje */
-#define D0 16
 #define D1 5
 #define D2 4
 #define D3 0
 #define D4 2
+#define D0 16
 #define D5 14
 #define D6 12
 #define D7 13
 #define D8 15
 
-#define LED D4
-#define PRZOD D5
-#define TYL D3
-#define LEWO D2
-#define PRAWO D1
-#define AUTO D8
 
-/* Wi-Fi */
-const char* SSID = "TOK-B8";
-const char* PASS = "uwvw9954";
+#define PRZOD D0
+#define TYL D3
+#define LEWO D6
+#define PRAWO D7
+#define AUTO D1
+#define Vplus D2
+#define Vminus D4
+#define LIGHT A0
 
 /* Blynk */
-#define BLYNK_PRINT Serial
-char auth[] = "3AG8OrjK4W-8UABPL3DeNeLSDqbmFLvb";
-char ssid[] = "TOK-B8";
-char pass[] = "uwvw9954";
+
